4484763da13f2b980983b3bf047aabd05a5b51aa
qbq <tobeqbq@gmail.com>
2016-03-01 21:17:32 +0800

    update quote and trading

--- js/quote/QuoteModel.js
+++ js/quote/QuoteModel.js
@@ -2,28 +2,31 @@
 
 define([], function() {
 
-	var QuoteModel = Backbone.Model.extend({
+    var QuoteModel = Backbone.Model.extend({
         // url: 'data/quote.json',
         url: 'http://v2.yundzh.com/quote/dyna',
         defaults: {
-            "Id": 888, 
-            "RepDataQuoteDynaSingle": [
-                {
-                    "Obj": "SH600000", 
-                    "Data": {
-                        "Id": "888888", 
-                        "ShiJian": "1234567890", 
-                        "ZhongWenJianCheng": "浦发银行",
-                        "ZuiXinJia": 16, 
-                        "ZhangDie": 1,
-                        "ZhangFu": -0.10,
-                        "ZuiGaoJia": 18, 
-                        "ZuiDiJia": 15, 
-                        "HuanShou": 0.05, 
-                        "ChengJiaoLiang": 123
+            "Counter": 1,
+            "Data": {
+                "Id": 888, 
+                "RepDataQuoteDynaSingle": [
+                    {
+                        "Obj": "SH600000", 
+                        "Data": {
+                            "Id": "888888", 
+                            "ShiJian": "1234567890", 
+                            "ZhongWenJianCheng": "浦发银行",
+                            "ZuiXinJia": 16, 
+                            "ZhangDie": 1,
+                            "ZhangFu": -0.10,
+                            "ZuiGaoJia": 18, 
+                            "ZuiDiJia": 15, 
+                            "HuanShou": 0.05, 
+                            "ChengJiaoLiang": 123
+                        }
                     }
-                }
-            ]
+                ]
+            }
         }
     });
 
